90f3f078c27dabfa4e7fdab35c2701376164f9bd
std-s23007 <s23007@std.it-college.ac.jp>
2025-07-17 11:24:25 +0900

    googleでログインできるようにした

--- src/firebase.js
+++ src/firebase.js
@@ -1,41 +1,21 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDug6k408E81owFyQNA98YjikBAqGlE7mM",
   authDomain: "pbl-c-54088.firebaseapp.com",
   projectId: "pbl-c-54088",
-  storageBucket: "pbl-c-54088.firebasestorage.app",
+  storageBucket: "pbl-c-54088.appspot.com",
   messagingSenderId: "44768519903",
   appId: "1:44768519903:web:6b8a002e5981bb1300ab6a",
-  measurementId: "G-D8B3QJG0G9"
+  measurementId: "G-D8B3QJG0G9",
 };
-// Firebaseの初期化
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
 
-// データ追加
-async function addStudent(studentId, name, password) {
-  try {
-    const docRef = await addDoc(collection(db, "students"), {
-      学籍番号: studentId,
-      氏名: name,
-      パスワード: password
-    });
-    console.log("ドキュメント追加成功 ID: ", docRef.id);
-  } catch (e) {
-    console.error("追加エラー: ", e);
-  }
-}
-
-// データ取得
-async function getStudent() {
-  const querySnapshot = await getDocs(collection(db, "users"));
-  querySnapshot.forEach((doc) => {
-    console.log(doc.id, " => ", doc.data());
-  });
-}
-
-addStudent();
-getStudent();
+export { auth, provider, db };
